d62b99614caf4f7ed2afae4737a1e46dba0576b0
Evaldo Klock <neto.nemesis@gmail.com>
2024-06-08 15:13:49 -0400

    feat: add translation

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,18 +1,20 @@
-import arMessages from './messages/ar.json';
-import frMessages from './messages/fr.json';
-import es419Messages from './messages/es_419.json';
-import zhcnMessages from './messages/zh_CN.json';
-import ptMessages from './messages/pt.json';
-import itMessages from './messages/it.json';
-import ukMessages from './messages/uk.json';
-import deMessages from './messages/de.json';
-import ruMessages from './messages/ru.json';
-import hiMessages from './messages/hi.json';
-import frCAMessages from './messages/fr_CA.json';
+import arMessages from './messages/ar.json'
+import deMessages from './messages/de.json'
+import es419Messages from './messages/es_419.json'
+import frMessages from './messages/fr.json'
+import frCAMessages from './messages/fr_CA.json'
+import hiMessages from './messages/hi.json'
+import itMessages from './messages/it.json'
+import ptMessages from './messages/pt.json'
+import ptBrMessages from './messages/pt_BR.json'
+import ruMessages from './messages/ru.json'
+import ukMessages from './messages/uk.json'
+import zhcnMessages from './messages/zh_CN.json'
 // no need to import en messages-- they are in the defaultMessage field
 
 const messages = {
   ar: arMessages,
+  'pt-br': ptBrMessages,
   'es-419': es419Messages,
   fr: frMessages,
   'zh-cn': zhcnMessages,
@@ -23,6 +25,6 @@ const messages = {
   'fr-ca': frCAMessages,
   ru: ruMessages,
   uk: ukMessages,
-};
+}
 
-export default messages;
+export default messages
